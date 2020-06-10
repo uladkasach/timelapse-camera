3ca93c24f3bb828cfd29818dd3b1d0acb2110105
Uladzimir Kasacheuski <uladkasach@users.noreply.github.com>
2020-06-10 15:37:54 -0500

    Update configureEnv.sh

--- rasberrypi/src/configureEnv.sh
+++ rasberrypi/src/configureEnv.sh
@@ -2,4 +2,5 @@
 pkill -f gphoto2;
 
 # see https://github.com/uladkasach/timelapse-camera/issues/2
-gphoto2 --set-config imagequality=2;
+gphoto2 --get-config imagequality ; # yeah, for some reason we do need to run this before we can set-config
+gphoto2 --set-config imagequality=2 ;
