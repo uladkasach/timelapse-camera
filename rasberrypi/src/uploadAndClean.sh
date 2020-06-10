ebd95a12ed895ae08c45a0a1866eda68eecdd9cf
Uladzimir Kasacheuski <uladkasach@users.noreply.github.com>
2020-06-10 13:16:01 -0500

    Update uploadAndClean.sh

--- rasberrypi/src/uploadAndClean.sh
+++ rasberrypi/src/uploadAndClean.sh
@@ -1,2 +1,3 @@
 # upload photo to the cloud and delete from machine
-aws s3 mv --recursive --exclude ".gitignore" ./shots/ s3://timelapse-camera-photos/shots
+BASEDIR=$(dirname "$0");
+aws s3 mv --recursive --exclude ".gitignore" $BASEDIR/shots/ s3://timelapse-camera-photos/shots
