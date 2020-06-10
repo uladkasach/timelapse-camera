4210239c404c794337d7020e586e75c31a75ae4a
Uladzimir Kasacheuski <uladkasach@users.noreply.github.com>
2020-06-10 12:56:22 -0500

    Update and rename every15Seconds.sh to captureEvery15SecondsAndThenClean.sh

--- rasberrypi/src/every15Seconds.sh
+++
@@ -1,6 +0,0 @@
-# since cron only runs at max once a minute, we need to have one command that runs these every 15 seconds (w/o waiting for prior to complete)
-# per https://askubuntu.com/a/805/357970
-(./captureUploadClean.sh) &
-(sleep 15 && ./captureUploadClean.sh) &
-(sleep 30 && ./captureUploadClean.sh) &
-(sleep 45 && ./captureUploadClean.sh); 
