157347e20e8ddb1028113297b7141c4a25acf9cb
Uladzimir Kasacheuski <uladkasach@users.noreply.github.com>
2020-06-10 13:18:02 -0500

    Update capture.sh

--- rasberrypi/src/capture.sh
+++ rasberrypi/src/capture.sh
@@ -1,3 +1,4 @@
 # take photo with camera and download it to device
-mkdir -p ./shots # create dir if dne
-gphoto2 --capture-image-and-download --filename './shots/%Y-%m-%dT%H:%M:%SZ.%f.%C'
+BASEDIR=$(dirname "$0");
+mkdir -p $BASEDIR/shots; # create dir if dne
+gphoto2 --capture-image-and-download --filename "$BASEDIR/shots/%Y-%m-%dT%H:%M:%SZ.%f.%C";
