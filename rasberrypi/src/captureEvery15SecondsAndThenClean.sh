51a595b5936087be2fbf5ef709e5ff5eae87a179
Uladzimir Kasacheuski <uladkasach@users.noreply.github.com>
2020-06-10 13:19:41 -0500

    Update captureEvery15SecondsAndThenClean.sh

--- rasberrypi/src/captureEvery15SecondsAndThenClean.sh
+++ rasberrypi/src/captureEvery15SecondsAndThenClean.sh
@@ -1,7 +1,10 @@
 # since cron only runs at max once a minute, we need to have one command that runs these every 15 seconds (w/o waiting for prior to complete)
 # per https://askubuntu.com/a/805/357970
-(./capture.sh) &
-(sleep 15 && ./capture.sh) &
-(sleep 30 && ./capture.sh) &
-(sleep 45 && ./capture.sh) & 
-(sleep 55 && ./uploadAndClean.sh); # only upload and clean once, after we've taken all the photos 
+
+BASEDIR=$(dirname "$0");
+
+($BASEDIR/capture.sh) &
+(sleep 15 && $BASEDIR/capture.sh) &
+(sleep 30 && $BASEDIR/capture.sh) &
+(sleep 45 && $BASEDIR/capture.sh) & 
+(sleep 55 && $BASEDIR/uploadAndClean.sh); # only upload and clean once, after we've taken all the photos 
