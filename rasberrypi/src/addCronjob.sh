30aa8372997decd04c45a869208fbff88a9fdfd3
Uladzimir Kasacheuski <uladkasach@users.noreply.github.com>
2020-06-10 14:59:09 -0500

    Update addCronjob.sh

--- rasberrypi/src/addCronjob.sh
+++ rasberrypi/src/addCronjob.sh
@@ -3,9 +3,8 @@
 crontab -l > mycron
 
 #echo new cron into cron file
+echo "@reboot /home/pi/timelapse/configureEnv.sh" >> mycron;
 echo "* * * * * /home/pi/timelapse/captureEvery15SecondsAndThenClean.sh" >> mycron;
-echo "@reboot pkill -f gphoto2 && gphoto2 --set-config imagequality=2" >> mycron; # see https://github.com/uladkasach/timelapse-camera/issues/1 and https://github.com/uladkasach/timelapse-camera/issues/2
-
 #install new cron file
 crontab mycron
 rm mycron
