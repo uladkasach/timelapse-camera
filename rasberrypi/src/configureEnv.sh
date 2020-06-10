2cc0828a920ac5a5f23c98337bedbe7df13b73df
Uladzimir Kasacheuski <uladkasach@users.noreply.github.com>
2020-06-10 14:59:49 -0500

    Create configureEnv.sh

---
+++ rasberrypi/src/configureEnv.sh
@@ -0,0 +1,5 @@
+# see https://github.com/uladkasach/timelapse-camera/issues/1
+pkill -f gphoto2;
+
+# see https://github.com/uladkasach/timelapse-camera/issues/2
+gphoto2 --set-config imagequality=2;
